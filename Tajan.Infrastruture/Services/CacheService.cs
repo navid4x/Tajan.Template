a8ceab51dd762099c99b1ebe9910f64291810d12
n.raghebifar <n.raghebifar@STATION415>
2025-09-30 10:44:51 +0330

    .

--- Tajan.Infrastruture/Services/CacheService.cs
+++ Tajan.Infrastruture/Services/CacheService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tajan.Infrastruture.Persistence.Logging
+namespace Tajan.Infrastructure.Persistense.Services
 {
-    internal class LoggerService
+    internal class CacheService
     {
     }
 }
